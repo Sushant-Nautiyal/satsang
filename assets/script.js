6498b233b28f5cc345e49bd8bd02ee50737094c3
Sushant Nautiyal <sushantnautiyal592@gmail.com>
2026-06-21 12:52:45 +0530

    adding assets and images

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+
+const observer=new IntersectionObserver(entries=>{
+entries.forEach(entry=>{
+if(entry.isIntersecting){entry.target.classList.add('active');}
+});
+});
+document.querySelectorAll('.reveal').forEach(el=>observer.observe(el));
